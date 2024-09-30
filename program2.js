5a1334e9e104f8ac97be6a9984ff71d15d55805a
Agyat2003 <0126cd211019@oriental.ac.in>
2024-09-30 15:16:23 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -12,6 +12,22 @@ var romanToInt = function(s) {
         'D': 500,
         'M': 1000
     };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+
+        // If current is smaller than next, subtract it, otherwise add it
+        if (next && current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
 };
 
 

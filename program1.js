c505ba3092a569641536c243e0643ade8c2b12be
Agyat2003 <0126cd211019@oriental.ac.in>
2024-09-30 15:12:37 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const dictionary = {
+        "(":")",
+        "{":"}",
+        "[":"]"
+        }
+    const stack = [];
+    for (let i =0; i< s.length; i++) {
+        let currentBracket = s[i]; 
+        let lastBracket = stack[stack.length-1]; 
+        if (currentBracket in dictionary) {
+            stack.push(dictionary[currentBracket])
+            continue;
+        }
+        if (currentBracket === lastBracket ) {
+            stack.pop();
+            continue;
+        }
+        return false;
+    }
+    return !stack.length;
 };
 
 module.exports = { isValid };
